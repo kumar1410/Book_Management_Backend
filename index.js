2934b18c1db6fb29d015bcf0407135c55ca74dd8
Rockstar Baghel <Rbaghel1410@gmail.com>
2025-01-17 11:04:38 +0530

    index

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-import app from "./app.js";
+import http from 'http';
+// import app from "./app.js";
 import { connectToDB } from "./src/database/dataSource.js";
 
 const PORT = process.env.PORT || 5000;
 
-const server = connectToDB()
+const server = http.createServer((req, res) => {
+  // Set the response header
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  // Write some text to the response
+  res.end('Welcome to my simple Node.js app!');
+});
+
+connectToDB()
   .then(() => {
-    return app.listen(PORT, () => {
+    server.listen(PORT, () => {
       console.log(`Server started at port ${PORT}`);
     });
   })
