653b14e6e4f75a17ff507bfd6abbde1a195e6641
Rockstar Baghel <Rbaghel1410@gmail.com>
2025-01-17 11:54:33 +0530

    index

--- index.js
+++ index.js
@@ -1,28 +1,15 @@
-import http from "http";
+import app from "./app.js";
 import { connectToDB } from "./src/database/dataSource.js";
 
 const PORT = process.env.PORT || 5000;
 
-// Define the request handler
-const requestHandler = (req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Welcome to my simple Node.js app!");
-};
-
-// Use an IIFE for initialization
-(async () => {
-  try {
-    await connectToDB();
-    console.log("Database connected!");
-
-    const server = http.createServer(requestHandler);
-
-    // Ensure listen is called only once
-    server.listen(PORT, () => {
-      console.log(`Server running at http://localhost:${PORT}`);
+connectToDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server started at port ${PORT}`);
     });
-  } catch (error) {
-    console.error("Error during initialization:", error.message);
-    process.exit(1); // Exit the process to prevent lingering issues
-  }
-})();
+  })
+  .catch((err) => {
+    console.error("Error during initialization:", err.message);
+    process.exit(1); // Exit the process on failure
+  });
